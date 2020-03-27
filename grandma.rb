cd95672b5d612b0541bbbccfb3706471ad8b023d
jamelia harrison <jameliaharrison@gmail.com>
2020-03-27 05:59:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase) 
-  if phrase != phrase.upcase
+def speak_to_grandma(says) 
+  if says != says.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  elsif says == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
   else
     return "NO, NOT SINCE 1938!"
